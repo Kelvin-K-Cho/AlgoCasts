f7cc778aa1c58630be8576a2eb7e9f8f2edfafdc
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-03-22 22:28:23 -0700

    Finish fibs

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -1,4 +1,3 @@
-// --- Directions
 // Print out the n-th entry in the fibonacci series.
 // The fibonacci series is an ordering of numbers where
 // each number is the sum of the preceeding two.
@@ -8,6 +7,19 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n, obj) {
+	obj = obj || {
+		0: 0,
+		1: 1
+	};
+
+	if (n === 0 || obj[n]) {
+		return obj[n];
+	}
+
+	let answer = fib(n - 1, obj) + fib(n - 2, obj);
+	obj[n] = answer;
+	return answer;
+}
 
 module.exports = fib;
